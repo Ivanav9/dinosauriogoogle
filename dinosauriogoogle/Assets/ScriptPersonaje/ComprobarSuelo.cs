43dd767fc5e6bc5451962c08596e86e8baeef872
Anbersan <quiroga.alejandra2002@gmail.com>
2023-12-19 13:19:27 +0100

    salto y correr

---
+++ dinosauriogoogle/Assets/ScriptPersonaje/ComprobarSuelo.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComprobarSuelo : MonoBehaviour
+{
+    public static bool estaEnSuelo;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        estaEnSuelo = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        estaEnSuelo = false;
+    }
+}
---
+++ dinosauriogoogle/Assets/ScriptPersonaje/personaje.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class personaje : MonoBehaviour
+{
+    [SerializeField] float vida;
+    [SerializeField] float velocidad;
+    [SerializeField] float alturaSalto;
+    [SerializeField] float alturaDobleSalto;
+    [SerializeField] bool puedeDobleSalto = true;
+    [SerializeField] Rigidbody2D rigidbody;
+    [SerializeField] Animator animator;
+    [SerializeField] SpriteRenderer spriteRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (ComprobarSuelo.estaEnSuelo)
+            {
+                rigidbody.AddForce(Vector2.up * alturaSalto);
+                puedeDobleSalto = true;
+            }
+            else if (!ComprobarSuelo.estaEnSuelo && puedeDobleSalto)
+            {
+                rigidbody.velocity = Vector2.zero;
+                rigidbody.AddForce(Vector2.up * alturaSalto);
+                puedeDobleSalto = false;
+            }
+        }
+        if (rigidbody.velocity.y > 0.1f || rigidbody.velocity.y < -0.1f)
+        {
+            animator.SetBool("estaEnSuelo", false);
+        }
+        else
+        {
+            animator.SetBool("estaEnSuelo", true);
+        }
+    }
+}
