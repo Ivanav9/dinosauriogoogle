f70ec34c03041f1b3642d79213e0a1221605e4be
Anbersan <quiroga.alejandra2002@gmail.com>
2023-12-19 14:14:08 +0100

    Tiempo Un minuto

--- dinosauriogoogle/Assets/Scripts/GameManager.cs
+++ dinosauriogoogle/Assets/Scripts/GameManager.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instancia;
     [SerializeField] public int puntuacionActual, mejorPuntuacion;
+    [SerializeField] float tiempo = 120;
+    [SerializeField] TMP_Text texto;
     // Start is called before the first frame update
+
     void Start()
     {
         mejorPuntuacion = PlayerPrefs.GetInt("mejorPuntuacion");
@@ -15,7 +19,12 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        tiempo -= Time.deltaTime;
+        int minutos = (int)tiempo / 60;
+        int segundos = (int)tiempo % 60;
+        Debug.Log(minutos + ":" + segundos);
+        string textoTiempo = $"{minutos:D2}:{segundos:D2}";
+        texto.text = textoTiempo;
     }
     public void Reset()
     {
