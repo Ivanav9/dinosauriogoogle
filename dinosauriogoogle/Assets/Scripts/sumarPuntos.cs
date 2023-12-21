27493c04a11720ce4f2767d6264243d532b85b9d
AnibalTheBoss <anibalherreronieto@gmail.com>
2023-12-21 10:14:18 +0100

    pequeña actualizacion

--- dinosauriogoogle/Assets/Scripts/sumarPuntos.cs
+++ dinosauriogoogle/Assets/Scripts/sumarPuntos.cs
@@ -15,7 +15,7 @@ public class sumarPuntos : MonoBehaviour
     {
         
     }
-    private void OnTriggerEnter2D(Collider other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.transform.tag == "SumarPuntos")
         {
