9427453c269da7efcd2f221e380acf36c91a7351
DESKTOP-CEBRCRH\milag <cfgsupermilahdc202223@gmail.com>
2023-12-21 10:15:22 +0100

    guardarenemigo2, crearenemigo2, guardarposicioninicial,guardarvelocidadinicial, creargameobjectenemigo2 seccion Ivan

---
+++ dinosauriogoogle/Assets/Scripts/enemigoivan.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using static UnityEngine.RuleTile.TilingRuleOutput;
+
+public class enemigoivan : MonoBehaviour
+{
+    [SerializeField] Vector3 posicionincial;
+    [SerializeField] Vector3 posicionMinima;
+    [SerializeField] Camera camara;
+    [SerializeField] float velocidad;
+    // Start is called before the first frame update
+    void Start()
+    {
+        camara = Camera.main;
+        posicionincial = transform.position;
+        posicionMinima = camara.ViewportToWorldPoint(new Vector2(0, 0));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.Translate(Vector3.left * velocidad * Time.deltaTime);
+        if (transform.position.x < posicionMinima.x)
+        {
+            transform.position = posicionincial;
+            velocidad += 1;
+        }
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.tag == "Perder")
+        {
+            //GameManager.Instancia.reiniciarNivel();
+            //transform.position = posicionincial;
+
+        }
+ 
+    }
+
+    public void iniciarenemigo()
+    {
+        transform.position = posicionincial;
+        velocidad += 1;
+    }
+}
+
+
+
+
