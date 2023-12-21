d7fbadd8d1b04b7a2a4bdc6f7998ace3d3e6ea29
Alejandra <quiroga.alejandra2002@gmail.com>
2023-12-21 09:46:22 +0100

    tiempo

--- dinosauriogoogle/Assets/Scripts/GameManager.cs
+++ dinosauriogoogle/Assets/Scripts/GameManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
@@ -8,7 +9,8 @@ public class GameManager : MonoBehaviour
     public int puntuacionActual, mejorPuntuacion;
     public GameObject texto, boton;
     public GameObject guardarJugador, guardarEnemigo;
-    public bool cronometro;
+    [SerializeField] float tiempo = 120;
+    [SerializeField] TMP_Text texto2;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,7 +20,12 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        tiempo -= Time.deltaTime;
+        int minutos = (int)tiempo / 60;
+        int segundos = (int)tiempo % 60;
+        Debug.Log(minutos + ":" + segundos);
+        string textoTiempo = $"{minutos:D2}:{segundos:D2}";
+        texto2.text = textoTiempo;
     }
     public void Reset()
     {
